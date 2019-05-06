9ea968980588bf458b92a0de3606aabc997d8888
TikaWorld <tikaworld0416@gmail.com>
2019-05-06 22:48:43 +0900

    Refactoring CheckListProblem

--- Algorithms/CheckList.c
+++ Algorithms/CheckList.c
@@ -6,6 +6,8 @@ int test() {
 	int list2[5] = { 1,1,2,3,4 };
 	int j = 0;
 
+	printf("%d", checkList(list1, list2));
+
 	for (int i = 0; i < 3; i++) {
 		chk = 0;
 		for (; j < 5; j++) {
@@ -23,3 +25,17 @@ int test() {
 }
 
 
+int checkList(int* list, int* unionList) {
+	int count = 0;
+	for (int i = 0; i < sizeof(*unionList) / sizeof(int); i++) {
+		printf("%d %d %d %d\n", list[count], unionList[i], sizeof(list), sizeof(*unionList));
+		if (list[count] == unionList[i]) {
+			count++;
+			if (count > (sizeof(list) / sizeof(int)))
+				return 0;
+		}
+	}
+	printf("%d\n", count);
+
+	return 1;
+}
--- Algorithms/Test.h
+++ Algorithms/Test.h
@@ -2,4 +2,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Test();
+int test();
